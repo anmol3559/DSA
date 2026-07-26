a56eb506c2c6633b7aed167eb6b9c761b32c58fa
anmol3559 <anmoltyagi3559@gmail.com>
2026-07-27 02:06:08 +0530

    Time: 15 ms (45.92%), Space: 48.4 MB (82.43%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -6,7 +6,7 @@ public:
         vector<int> ans;
         
         for( int i=0 ; i<nums.size()-1 ; i++ ){
-            if( i+1<nums.size() && nums[i]==nums[i+1] ){
+            if( nums[i]==nums[i+1] ){
                ans.push_back(nums[i]);            
                }
         }
