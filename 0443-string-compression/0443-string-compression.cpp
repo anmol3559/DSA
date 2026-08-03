08995d93110b3852e59b12550c66b8e932d45716
anmol3559 <anmoltyagi3559@gmail.com>
2026-08-04 05:10:50 +0530

    Time: 0 ms (100%), Space: 13.9 MB (14.73%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int i = 0;
+        int ansIndex = 0;
+         while(i < chars.size()){
+            int j = i+1;
+            while(j < chars.size() && chars[i] == chars[j]){
+                j++;
+            }
+            //new char mila ya arry khtm ho gya
+            chars[ansIndex++] = chars[i];
+
+            int count = j - i;
+            if( count > 1){
+                string cnt = to_string(count);
+                for( char ch : cnt){
+                    chars[ansIndex++] = ch;
+                }
+            }
+                i=j;
+         }
+         return ansIndex;
+    }
+};
