c49ee80c7e1ae7dc7676eb504131716c681e8ea2
anmol3559 <anmoltyagi3559@gmail.com>
2026-08-06 01:20:42 +0530

    Time: 43 ms (90.38%), Space: 18.8 MB (39.64%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -5,7 +5,7 @@ public:
         int start = 0;
         int end = matrix[0].size() - 1;
 
-        while(start<matrix.size() && end>=0){
+        while(end>=0 && start<matrix.size() ){
 
             if( matrix[start][end] == target){
                 return 1;
