f72d37ef0c79315c512b20fec2b84f56868ea4b1
anmol3559 <anmoltyagi3559@gmail.com>
2026-08-07 00:55:38 +0530

    Time: 713 ms (5%), Space: 12.6 MB (91.79%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -8,7 +8,6 @@ public:
         for(int i=2; i<n; i++){
             if(prime[i]){
                 cnt++;
-                prime[i] = false;
             }
             for(int j=2*i; j<n; j=j+i){
                 prime[j] = false;
