5ad43aef075986545a16f023827702c0025905c9
anmol3559 <anmoltyagi3559@gmail.com>
2026-08-06 20:34:04 +0530

    Time: 0 ms (100%), Space: 64.7 MB (39.86%) - LeetHub

--- 2643-row-with-maximum-ones/2643-row-with-maximum-ones.cpp
+++ 2643-row-with-maximum-ones/2643-row-with-maximum-ones.cpp
@@ -7,9 +7,7 @@ public:
         for(int i=0; i<mat.size(); i++){
             int count = 0;
             for(int j=0; j<mat[0].size(); j++){
-                if(mat[i][j] == 1){
-                    count++;
-                }
+                if(mat[i][j] == 1) count++;
             }
             if( count > max_1s){
                 max_1s = count;
