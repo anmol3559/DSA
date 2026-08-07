11839df7586322cd2caad81a579f884639b5ad7c
anmol3559 <anmoltyagi3559@gmail.com>
2026-08-08 03:28:11 +0530

    Time: 0 ms (100%), Space: 27.2 MB (53.59%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -3,8 +3,10 @@ public:
     void reverseString(vector<char>& s) {
         int st = 0;
         int end = s.size() - 1;
-        while(st < end){
-            swap( s[st++] , s[end--]);
+        while(st <= end){
+            swap( s[st] , s[end]);
+            st++;
+            end--;
         }
     }
 };
