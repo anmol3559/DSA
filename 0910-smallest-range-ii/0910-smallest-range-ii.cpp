d905c18419e58713f0f3b8d9e4440b77eb33403e
anmol3559 <anmoltyagi3559@gmail.com>
2026-08-09 01:30:18 +0530

    Time: 4 ms (47.01%), Space: 19.2 MB (76.23%) - LeetHub

---
+++ 0910-smallest-range-ii/0910-smallest-range-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int smallestRangeII(vector<int>& nums, int k) {
+    
+    if( nums.size() == 1) return 0;
+
+    sort(nums.begin() , nums.end());
+    int ans = nums[nums.size() - 1] - nums[0];
+
+    int mini , maxi;
+    for(int i=1; i<nums.size(); i++){
+      
+
+        mini = min( nums[0] + k , nums[i] - k);
+        maxi = max( nums[nums.size() - 1] - k , nums[i-1] + k);
+
+        ans = min( ans , maxi - mini);
+    }
+
+    return ans;
+
+    }
+};
