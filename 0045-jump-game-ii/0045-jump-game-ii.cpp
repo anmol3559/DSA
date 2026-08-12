4faebea1651a09cf52f69c6bf5f6f0fe5a8f214c
anmol3559 <anmoltyagi3559@gmail.com>
2026-08-13 03:32:44 +0530

    Time: 0 ms (100%), Space: 20.5 MB (79.19%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-     int jump = 0;
-     int current_end =0;
-     int farthest = 0;
-     for(int i=0; i<nums.size()-1; i++){
-        farthest = max(farthest , i + nums[i] );
-        if(i == current_end){
-            jump++;
-            current_end = farthest;
+        int jump = 0;
+        int current_end = 0;
+        int farthest = 0;
+        for(int i=0; i<nums.size()-1; i++){
+            farthest = max(farthest , i+ nums[i]);
+            if( i == current_end){
+                jump++;
+                current_end = farthest;
+            }
         }
-     } 
-     return jump;  
+        return jump;
     }
 };
