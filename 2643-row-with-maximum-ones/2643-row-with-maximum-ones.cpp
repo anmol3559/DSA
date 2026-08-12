7f974d838a8bb51a5b65696e173b05d0895200f0
anmol3559 <anmoltyagi3559@gmail.com>
2026-08-13 03:41:50 +0530

    Time: 0 ms (100%), Space: 64.7 MB (69.49%) - LeetHub

--- 2643-row-with-maximum-ones/2643-row-with-maximum-ones.cpp
+++ 2643-row-with-maximum-ones/2643-row-with-maximum-ones.cpp
@@ -2,15 +2,17 @@ class Solution {
 public:
     vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
         int max_row = 0;
-        int max_1s = INT_MIN;
+        int max_1s = 0;
         for(int i=0; i<mat.size(); i++){
             int count = 0;
             for(int j=0; j<mat[0].size(); j++){
-                if(mat[i][j] == 1) count++;
+                if( mat[i][j] == 1){
+                    count ++;
+                }
             }
             if( count > max_1s){
-                max_1s = count;
                 max_row = i;
+                max_1s = count;
             }
         }
         return {max_row , max_1s};
