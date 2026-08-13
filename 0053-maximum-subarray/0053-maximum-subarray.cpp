4ba8b67a4aa6a3d81c5d16a9e04290683260e16f
anmol3559 <anmoltyagi3559@gmail.com>
2026-08-13 12:08:32 +0530

    Time: 4 ms (10.52%), Space: 71.7 MB (53.48%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-       int max_sum = INT_MIN;
-       int current_sum = 0;
-       for(int i=0; i<nums.size(); i++){
-        current_sum += nums[i];
-        if(current_sum > max_sum){
-            max_sum = current_sum;
+        int current_sum = 0;
+        int max = INT_MIN;
+
+        for(int i=0; i<nums.size(); i++){
+            current_sum += nums[i]; 
+             if(current_sum > max){
+                max = current_sum;
+             }
+             if(current_sum < 0){
+               current_sum = 0;
+             }
         }
-        if(current_sum < 0 ){
-            current_sum = 0; 
-        }
-       } 
-       return max_sum;
+       return max;
     }
 };
