d36a65d09f86dd101ba680af9da3ec9248fc6691
anmol3559 <anmoltyagi3559@gmail.com>
2026-08-15 02:02:24 +0530

    Time: 0 ms (100%), Space: 12.7 MB (7.41%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,17 +1,23 @@
 class Solution {
+    int binarySearch(vector<int>& nums, int st, int end){
+        int mid = st + (end - st)/2;
+        if(st >= end) return st;
+        if(nums[mid] < nums[mid+1]) return binarySearch(nums , mid + 1, end);
+        else return binarySearch(nums, st , mid);
+    }
 public:
     int findPeakElement(vector<int>& nums) {
         
         int st = 0 ;
         int end = nums.size() - 1;
         
-        while(st<end){
+        // while(st<end){
 
-            int mid = st + (end - st)/2;
-            if(nums[mid] < nums[mid + 1]) st = mid + 1;
-            else end = mid;
-            
-        }
-        return st ;
+            // int mid = st + (end - st)/2;
+            // if(nums[mid] < nums[mid + 1]) st = mid + 1;
+            // else end = mid;
+        int ans = binarySearch(nums, st , end);
+        // }
+        return ans ;
     }
 };
