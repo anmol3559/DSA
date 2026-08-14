474f4093985ced7ff71d8e476a7eb7299e4e0fc4
anmol3559 <anmoltyagi3559@gmail.com>
2026-08-15 01:36:41 +0530

    Time: 0 ms (100%), Space: 63.7 MB (11%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -1,18 +1,19 @@
 class Solution {
+    int binarysearch(vector<int>& arr , int start , int end){
+        if(start>end) return start;
+        int mid = start + (end - start)/2;
+        if(arr[mid] < arr[mid + 1]){
+          return binarysearch(arr , mid+1 , end);
+        }
+        else{
+            return binarysearch(arr, start , mid - 1);
+        }
+    }
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
         int start = 0;
         int end = arr.size() - 1;
-        int mid = start + (end - start)/2;
-        while(start<end){
-            if(arr[mid] < arr[mid + 1]){
-                start = mid + 1;
-            }
-            else{
-                end = mid;
-            }
-            mid = start + (end - start)/2;
-        }
-        return start;
+        int ans = binarysearch(arr, start , end);
+        return ans;
     }
 };
