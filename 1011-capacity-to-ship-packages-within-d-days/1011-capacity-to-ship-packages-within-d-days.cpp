11c89ed54f92146c24e1990609f49692cb96ee48
anmol3559 <anmoltyagi3559@gmail.com>
2026-08-18 00:41:18 +0530

    Time: 19 ms (9.24%), Space: 34.9 MB (88.86%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,20 +1,17 @@
 class Solution {
-    bool isPossible(vector<int>& weights, int days, int mid){
-        int daysCount = 1;
-        int weightsSum = 0;
-        for(int i=0; i<weights.size(); i++){
-            if( weightsSum + weights[i] <= mid){
-                weightsSum += weights[i];
+    bool isPossible(const vector<int>& weights, int days, int mid, int index, int currentSum, int daysCount){
+        if( index == weights.size()) return true;
+         if(weights[index] > mid) return false;
+
+         if(currentSum + weights[index] <= mid){
+            return isPossible(weights, days, mid, index + 1, currentSum + weights[index], daysCount);
+         }
+         else{
+            if(daysCount + 1 > days){
+                return false;
             }
-            else{
-                daysCount ++;
-                if( daysCount > days || weights[i] > mid){
-                    return false;
-                }
-                weightsSum = weights[i];
-            }
-        }
-        return true;
+            return isPossible(weights, days, mid, index + 1, weights[index], daysCount + 1);
+         }
     }
 public:
     int shipWithinDays(vector<int>& weights, int days) {
@@ -22,15 +19,17 @@ public:
         int sum = 0;
         for(int i=0; i<weights.size(); i++){
             sum += weights[i];
+            s = max(s , weights[i]);
         }
+
         int e = sum;
         int ans = -1;
         int mid = s + (e - s)/2;
-
         while(s <= e){
-            if( isPossible(weights, days, mid)){
+
+            if(isPossible(weights , days ,mid , 0, 0, 1)){
                 ans = mid;
-                e = mid -1;
+                e = mid - 1;
             }
             else{
                 s = mid + 1;
