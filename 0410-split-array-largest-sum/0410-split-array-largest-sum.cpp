59044bf13e11cf52a6f2a44b9ecee1c23c959215
anmol3559 <anmoltyagi3559@gmail.com>
2026-08-18 23:40:13 +0530

    Time: 0 ms (100%), Space: 10.5 MB (43.64%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,41 +1,39 @@
 class Solution {
-    bool largest(vector<int>& nums, int k, int mid){
-      int integerCount = 1;
-      int nums_sum = 0;
-      for(int i=0; i<nums.size(); i++){
-        if(nums_sum + nums[i] <= mid){
-            nums_sum += nums[i];
-        }
-        else{
-            integerCount++;
-            if(integerCount > k || nums[i] > mid ){
-                return false;
-            }
+    bool canSplit(vector<int>& nums, int k, int mid) {
+        int integerCount = 1;
+        int nums_sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums_sum + nums[i] <= mid) {
+                nums_sum += nums[i];
+            } else {
+                integerCount++;
+                if (integerCount > k || nums[i] > mid) {
+                    return false;
+                }
                 nums_sum = nums[i];
+            }
         }
-      }
-      return true;
+        return true;
     }
+
 public:
     int splitArray(vector<int>& nums, int k) {
         int s = 0;
         int sum = 0;
-        for(int i=0; i<nums.size(); i++){
+        for (int i = 0; i < nums.size(); i++) {
             sum += nums[i];
             s = max(s, nums[i]);
         }
         int e = sum;
         int ans = -1;
-        while(s <= e){
-            int mid = s + (e - s)/2;
-            if(largest(nums , k , mid)){
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (canSplit(nums, k, mid)) {
                 ans = mid;
                 e = mid - 1;
-            }
-            else{
+            } else
                 s = mid + 1;
-            }
-        } 
+        }
         return ans;
     }
 };
