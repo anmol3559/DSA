f32d33be5cb69565b08d070e061697a26b820da9
anmol3559 <anmoltyagi3559@gmail.com>
2026-08-21 01:45:13 +0530

    Time: 0 ms (100%), Space: 15.3 MB (36.82%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -5,20 +5,23 @@ public:
         int e = nums.size() - 1;
         while(s <= e){
             int mid = s + (e - s)/2;
-            if(nums[mid] == target)
+            if(nums[mid] == target){
                 return mid;
-            if(nums[s] <= nums[mid]){
-                if(target >= nums[s] && target < nums[mid])
-                    e = mid - 1;
-                else
-                    s = mid + 1;
             }
-           else{
-            if(target > nums[mid] && target <= nums[e])
-                s = mid + 1;
-            else 
-                e = mid - 1;
-           }
+            if(nums[s] <= nums[mid]){
+                if(target >= nums[s] && nums[mid] > target){
+                    e = mid - 1;
+                }else{
+                    s = mid + 1;
+                }
+            }
+            else{
+                if(target > nums[mid] && target <= nums[e]){
+                    s = mid + 1;
+                }else{
+                    e = mid - 1;
+                }
+            }
         }
         return -1;
     }
