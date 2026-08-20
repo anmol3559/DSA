16fb47eb19f26b2fbab31b4e0fb3dbb20a0a7d63
anmol3559 <anmoltyagi3559@gmail.com>
2026-08-21 02:32:00 +0530

    Time: 10 ms (39.56%), Space: 23 MB (56.86%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    bool canEat(vector<int>& piles, int h, int k){
+        long long totalHours = 0;
+        for(int i = 0; i < piles.size(); i++){
+            totalHours += (piles[i] + k - 1) / k;
+        }
+        return totalHours <= h;
+    }
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int s = 1;
+        int e = 0;
+        for(int i=0; i<piles.size(); i++){
+            e = max(e, piles[i]);
+        }
+        int ans = -1;
+        while(s <= e){
+            int mid = s + (e - s)/2;
+            if(canEat(piles, h, mid)){
+                ans = mid;
+                e = mid - 1;
+            }
+            else{
+                s = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
